4b2d7ddb1bda8f44d0e9ea2f1508f0b532ff9a33
incesamsul <ince.samsul89@gmail.com>
2023-05-28 15:32:04 +0800

    fix link

--- components/nav.php
+++ components/nav.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-expand-sm navbar-light p-4 fixed-top">
     <div class="container">
-        <a class="navbar-brand " href="#"><strong>Samtam Tech</strong></a>
+        <a class="navbar-brand " href="?page=home"><strong>Samtam Tech</strong></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
             <ul class="navbar-nav ">
                 <li class="nav-item mx-3">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="?page?home">Home</a>
                 </li>
                 <li class="nav-item mx-3">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="#footer">About</a>
                 </li>
                 <li class="nav-item mx-3">
                     <a class="nav-link" href="?page=portfolio">Portfolio</a>
                 </li>
                 <li class="nav-item mx-3">
-                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="#">Client</a>
                 </li>
                 <li class="nav-item mx-3">
                     <a class="nav-link" href="#" id="toggleMode">
